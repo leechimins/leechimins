21400edcce96874f519ef26315d6c3e211800509
이지민 <145891038+leechimins@users.noreply.github.com>
2024-10-15 11:48:14 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.c
@@ -1,23 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main(void) {
-	int N, two = 0, five = 0, ten = 0;
+	int N, ten = 0;
 	scanf("%d", &N);
 
-	for (int i = 1; i <= N; i++) {
-		int num = i;
-		while ((num % 2) == 0) {
-			num /= 2;
-			two++;
-		}
-		while ((num % 5) == 0) {
-			num /= 5;
-			five++;
-		}
-	}
-	while (two && five) {
-		ten++; two--; five--;
-	}
+	for (; N >= 5; N /= 5) ten += N / 5;
 
 	printf("%d", ten);
 
