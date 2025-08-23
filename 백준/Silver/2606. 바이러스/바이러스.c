7541b956437f751bdb7473d7c16c1b8a6af106cc
이지민 <145891038+leechimins@users.noreply.github.com>
2025-08-23 10:37:26 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int computer[101][101] = { { 0 }, { 0, 1 } };
+	int N, N2, M, c1, c2, result;
+
+	scanf("%d", &N);
+	scanf("%d", &M);
+	N2 = N * (N + 1);
+
+	while (M--) {
+		scanf("%d %d", &c1, &c2);
+
+		computer[c1][c2] = 1;
+		computer[c2][c1] = 1;
+	}
+
+	for (int i = 0; i < N2; i++) {
+		if (computer[i % N + 1][i % N + 1] == 0)
+			continue;
+
+		for (int j = 1; j <= N; j++) {
+			if (computer[i % N + 1][j] == 1)
+				computer[j][j] = 2;
+		}
+	}
+
+	result = N - 1;
+	for (int i = 1; i <= N; i++)
+		if (computer[i][i] == 0)
+			result--;
+
+	printf("%d", result);
+
+	return 0;
+}
