3b2084b3ca4c1caf2310bbdead7d5033b45acdee
이지민 <145891038+leechimins@users.noreply.github.com>
2025-08-24 13:02:58 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int N;
+	int result[1001] = { 1, 1 };
+	scanf("%d", &N);
+
+	for (int i = 2; i <= N; i++)
+		result[i] = (result[i - 2] + result[i - 1]) % 10007;
+
+	printf("%d", result[N]);
+
+	return 0;
+}
